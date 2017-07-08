07bb32244302f768adb354f9aa1b96caeeb90256
mosapeizer <k471352@gmail.com>
2017-07-08 23:20:26 +0800

    Fix: api url

--- app/api/Chat.php
+++ app/api/Chat.php
@@ -5,7 +5,7 @@ use Moz\Core\Curl;
 
 class Chat
 {
-    const URL = "https://api.slack.com/methods/chat.";
+    const URL = 'https://slack.com/api/chat.';
 
     // https://api.slack.com/methods/chat.postMessage
     public static function postMessage($channel, $text, $option=[])
